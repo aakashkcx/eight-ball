0c805f9bb2c1299f8b5299f1cfa1de8ba9a5fa9e
Aakash Kc <aakashkc.x@gmail.com>
2018-10-31 19:05:06 +0000

    custom authentication

--- authentication.js
+++ authentication.js
@@ -1,3 +1,30 @@
 'use strict';
 
-module.exports = {}
+// Components
+
+const User = require('./models/User')
+
+var authentication = function(req, res, next) {
+
+    if (req.session.user_id) {
+
+        req.user_id = req.session.user_id;
+        req.authenticated = true;
+
+        User.findById(req.user_id, false, (err, user) => {
+            if (!err) {
+                req.user = user;
+                res.locals.users = req.user;
+                next();
+            }
+        });
+
+    } else {
+
+        req.authenticated = false;
+        next();
+
+    }
+};
+
+module.exports = authentication;
