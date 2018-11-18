d84b58723cb2a2c0b78813076f665ec9b79beb39
Aakash Kc <aakashkc.x@gmail.com>
2018-11-18 16:50:28 +0000

    findByUsername

--- models/User.js
+++ models/User.js
@@ -1,36 +1,51 @@
 "use strict";
 
 // Dependencies
+const bcrypt = require('bcryptjs');
+
+// Imports
 const database = require("../database");
 
 const User = {};
 
 User.create = function (user, callback) {
 
-    let sql = `INSERT INTO user (username, email, password, firstname, lastname) VALUES (?, ?, ?, ?, ?);`;
-
-    database.run(sql, [user.username, user.email, user.password, user.firstname, user.lastname], function (err) {
-        callback(err, this.lastID);
+    bcrypt.hash(user.password, 10, (err, hash) => {
+        if (!err) {
+            let sql = `INSERT INTO user (username, email, password, firstname, lastname) VALUES (?, ?, ?, ?, ?);`;
+            database.run(sql, [user.username, user.email, hash, user.firstname, user.lastname], function (err) {
+                callback(err, this.lastID);
+            });
+        } else {
+            callback(err, null);
+        }
     });
 
 };
 
-User.findById = function (id, includePassword, callback) {
+User.findUserById = function (id, callback) {
 
-    let sql = `SELECT id, username, email, ${includePassword ? 'password, ' : ''}firstname, lastname, rating FROM user WHERE id = ?`;
-
+    let sql = `SELECT id, username, email, firstname, lastname, rating FROM user WHERE id = ?;`;
     database.get(sql, id, (err, user) => {
-        callback(err, user)
+        callback(err, user);
     });
 
 };
 
-User.findByUsername = function (username, includePassword, callback) {
+User.findIdByUsername = function (username, callback) {
 
-    let sql = `SELECT id, username, email, ${includePassword ? 'password, ' : ''}firstname, lastname, rating FROM user WHERE username = ?`;
-
+    let sql = `SELECT id FROM user WHERE username = ?;`;
     database.get(sql, username, (err, user) => {
-        callback(err, user)
+        callback(err, user.id);
+    });
+
+};
+
+User.findPasswordById = function (id, callback) {
+
+    let sql = `SELECT password FROM user WHERE id = ?;`;
+    database.get(sql, id, (err, user) => {
+        callback(err, user.password);
     });
 
 };
