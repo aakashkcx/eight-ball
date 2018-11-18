e04e8a6077dcf51910d7366683d89348a0b67964
Aakash Kc <aakashkc.x@gmail.com>
2018-11-18 16:49:18 +0000

    moved query

--- database.js
+++ database.js
@@ -9,7 +9,7 @@ const database = new sqlite.Database('./database.db');
 
 database.serialize(() => {
 
-    database.run(`CREATE TABLE IF NOT EXISTS user (
+    let sql = `CREATE TABLE IF NOT EXISTS user (
         id INTEGER PRIMARY KEY NOT NULL UNIQUE,
         username TEXT NOT NULL UNIQUE,
         email TEXT NOT NULL UNIQUE,
@@ -17,8 +17,10 @@ database.serialize(() => {
         firstname TEXT NOT NULL,
         lastname TEXT NOT NULL,
         rating INTEGER DEFAULT 1500
-    )`);
-    
+    );`;
+
+    database.run(sql);
+
 });
 
 module.exports = database;
