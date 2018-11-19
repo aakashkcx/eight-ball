e595fcffaca7c1d427a3913f831e9ee58a0773d0
Aakash KC <Aakash.Kc@saintolaves.net>
2018-11-19 10:28:33 +0000

    ;

--- game/Queue.js
+++ game/Queue.js
@@ -3,25 +3,25 @@
 const Queue = function() {
     this._queue = [];
     this.length = 0;
-}
+};
 
 Queue.prototype.enqueue = function(player) {
     this._queue.push(player);
     this.length++;
     player.inQueue = true;
-}
+};
 
 Queue.prototype.dequeue = function() {
     let player = this._queue.shift();
     this.length--;
     player.inQueue = false;
     return player;
-}
+};
 
 Queue.prototype.remove = function(player) {
     this._queue.splice(this._queue.indexOf(player), 1);
     this.length--;
     player.inQueue = false;
-}
+};
 
 module.exports = Queue;
