361b3162509c71dddad3c38c96efc26d5198b7f0
Aakash KC <Aakash.Kc@saintolaves.net>
2018-11-26 13:38:22 +0000

    remove length property

--- game/Queue.js
+++ game/Queue.js
@@ -1,26 +1,28 @@
 'use strict';
 
-const Queue = function() {
+const Queue = function () {
     this._queue = [];
-    this.length = 0;
 };
 
-Queue.prototype.enqueue = function(player) {
+Queue.prototype = {
+    get length() {
+        return this._queue.length;
+    }
+};
+
+Queue.prototype.enqueue = function (player) {
     this._queue.push(player);
-    this.length++;
     player.inQueue = true;
 };
 
-Queue.prototype.dequeue = function() {
+Queue.prototype.dequeue = function () {
     let player = this._queue.shift();
-    this.length--;
     player.inQueue = false;
     return player;
 };
 
-Queue.prototype.remove = function(player) {
+Queue.prototype.remove = function (player) {
     this._queue.splice(this._queue.indexOf(player), 1);
-    this.length--;
     player.inQueue = false;
 };
 
