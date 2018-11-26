3d265602dae28dba13e9d9109b67561f620b9984
Aakash KC <Aakash.Kc@saintolaves.net>
2018-11-26 13:37:57 +0000

    update socket

--- game/Player.js
+++ game/Player.js
@@ -1,14 +1,15 @@
 'use strict';
 
 // Player constructor
-const Player = function(socket) {
+const Player = function (socket) {
 
     this.id = socket.session.user_id;
     this.username = socket.session.user.username;
     this.socket = socket;
     this.inQueue = false;
     this.inGame = false;
-    this.game = null;
+
+    socket.player = this;
 
 };
 
