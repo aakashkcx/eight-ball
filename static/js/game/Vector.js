e11e4d2ea32b117e2a271b4a0d2bc573fe567198
Aakash Kc <aakashkc.x@gmail.com>
2019-02-22 23:00:41 +0000

    add methods

--- static/js/game/Vector.js
+++ static/js/game/Vector.js
@@ -10,3 +10,18 @@ const Vector = function (x = 0, y = 0) {
 Vector.add = function (vector1, vector2) {
     return new Vector(vector1.x + vector2.x, vector1.y + vector2.y);
 };
+
+// Vector subtraction static method
+Vector.subtract = function (vector1, vector2) {
+    return new Vector(vector1.x - vector2.x, vector1.y - vector2.y);
+};
+
+// Vector multiplication static method
+Vector.multiply = function (vector, scalar) {
+    return new Vector(vector.x * scalar, vector.y * scalar);
+};
+
+// Vector division static method
+Vector.divide = function (vector, scalar) {
+    return new Vector(vector.x / scalar, vector.y / scalar);
+};
