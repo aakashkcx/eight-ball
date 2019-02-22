0ec49279e5c2db5341be7ee44c23788bda1c93dd
Aakash Kc <aakashkc.x@gmail.com>
2019-02-22 23:19:42 +0000

    clean

--- game/Player.js
+++ game/Player.js
@@ -3,15 +3,14 @@
 // Player class constructor
 const Player = function (socket) {
 
+    this.socket = socket;
+    socket.player = this;
+
     this.id = socket.request.session.user_id;
     this.username = socket.request.session.user.username;
 
     this.inQueue = false;
     this.inGame = false;
-    this.playerNum = null;
-
-    this.socket = socket;
-    socket.player = this;
 
 };
 
